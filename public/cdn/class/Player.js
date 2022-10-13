0b033aa8759b71a2f4091bb389d82f1f8a726549
godmodeD <djlevy26@gmail.com>
2022-10-12 20:54:59 -0400

    front end player

---
+++ public/cdn/class/Player.js
@@ -0,0 +1,38 @@
+class Player {
+    constructor(name, id, x, y, PIXI, APP) {
+        this.name = name;
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.PIXI = PIXI;
+        this.APP = APP;
+        this.container = new PIXI.Container();
+
+        this.APP.stage.addChild(this.container);
+
+        this.nameText = new PIXI.Text(this.name, {
+            fontFamily: 'Arial',
+            fontSize: 24,
+            fill: "white",
+            align: 'right'
+        });
+        this.nameText.anchor.set(0.5, 0.5);
+        this.nameText.position.set(0, -25);
+
+        this.container.addChild(this.nameText);
+
+        this.sprite = PIXI.Sprite.from('cdn/sprites/cursor-icon.png');
+        this.sprite.scale.x = 0.04;
+        this.sprite.scale.y = 0.04;
+        this.container.addChild(this.sprite);
+
+    }
+
+    move(x, y) {
+        this.x = x;
+        this.y = y;
+        this.container.position.set(x, y);
+    }
+}
+
+export default Player;
